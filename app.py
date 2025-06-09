0e5b415c65c463d9c2607ee716f952fd9c788a9f
Bumbook125195 <ic231250@edu.okinawa-ct.ac.jp>
2025-06-09 12:25:34 +0900

    Add file: app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 import google.generativeai as genai
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
@@ -9,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_html():
-    return render_template('index.html')
+    now = datetime.datetime.now()
+    return render_template('index.html', current_time=now)
 
 if __name__ == '__main__':
     app.run(debug=True)
